fd553fb4d1d46a5322d5b2b9ce350e4589c63d36
renydiaz02 <89815150+renydiaz02@users.noreply.github.com>
2022-09-28 19:54:38 -0400

    modificando la base de datos agregando abstract val articulosDao: ArticulosDao

--- app/src/main/java/edu/ucne/p1_reny/data/EntidadDb.kt
+++ app/src/main/java/edu/ucne/p1_reny/data/EntidadDb.kt
@@ -2,14 +2,17 @@ package edu.ucne.p1_reny.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.p1_reny.data.dao.ArticulosDao
 import edu.ucne.p1_reny.model.Articulos
 
 @Database(
     entities = [
-        Articulos :: class
+        Articulos::class
     ],
     exportSchema = false,
     version = 2
 )
-abstract class EntidadDb : RoomDatabase (){
+abstract class EntidadDb : RoomDatabase() {
+    abstract val articulosDao: ArticulosDao
+
 }
